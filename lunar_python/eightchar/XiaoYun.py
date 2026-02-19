8b89490e7ec6c8d96445335aec5f232130d69c5f
LemonDrug <15868633@qq.com>
2026-02-19 13:22:35 +0800

    Refactor getGanZhi to use precomputed value

--- lunar_python/eightchar/XiaoYun.py
+++ lunar_python/eightchar/XiaoYun.py
@@ -14,6 +14,17 @@ class XiaoYun:
         self.__year = da_yun.getStartYear() + index
         self.__age = da_yun.getStartAge() + index
         self.__forward = forward
+        # 预计算干支
+        offset = LunarUtil.getJiaZiIndex(self.__lunar.getTimeInGanZhi())
+        add = self.__index + 1
+        if da_yun.getIndex() > 0:
+            add += da_yun.getStartAge() - 1
+        offset += add if self.__forward else -add
+        size = len(LunarUtil.JIA_ZI)
+        while offset < 0:
+            offset += size
+        offset %= size
+        self.__ganZhi = LunarUtil.JIA_ZI[offset]
 
     def getIndex(self):
         return self.__index
@@ -29,27 +40,18 @@ class XiaoYun:
         获取干支
         :return: 干支
         """
-        offset = LunarUtil.getJiaZiIndex(self.__lunar.getTimeInGanZhi())
-        add = self.__index + 1
-        if self.__daYun.getIndex() > 0:
-            add += self.__daYun.getStartAge() - 1
-        offset += add if self.__forward else -add
-        size = len(LunarUtil.JIA_ZI)
-        while offset < 0:
-            offset += size
-        offset %= size
-        return LunarUtil.JIA_ZI[offset]
+        return self.__ganZhi
 
     def getXun(self):
         """
         获取所在旬
         :return: 旬
         """
-        return LunarUtil.getXun(self.getGanZhi())
+        return LunarUtil.getXun(self.__ganZhi)
 
     def getXunKong(self):
         """
         获取旬空(空亡)
         :return: 旬空(空亡)
         """
-        return LunarUtil.getXunKong(self.getGanZhi())
+        return LunarUtil.getXunKong(self.__ganZhi)
