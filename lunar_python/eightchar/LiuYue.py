411346db46019f4ae7dfce9ffad88ef4e2234d68
LemonDrug <15868633@qq.com>
2026-02-19 13:22:09 +0800

    Precompute GanZhi in constructor and simplify methods

--- lunar_python/eightchar/LiuYue.py
+++ lunar_python/eightchar/LiuYue.py
@@ -11,6 +11,21 @@ class LiuYue:
     def __init__(self, liu_nian, index):
         self.__liuNian = liu_nian
         self.__index = index
+        # 预计算干支
+        year_gan = liu_nian.getGanZhi()[:1]
+        if "甲" == year_gan or "己" == year_gan:
+            offset = 2
+        elif "乙" == year_gan or "庚" == year_gan:
+            offset = 4
+        elif "丙" == year_gan or "辛" == year_gan:
+            offset = 6
+        elif "丁" == year_gan or "壬" == year_gan:
+            offset = 8
+        else:
+            offset = 0
+        gan = LunarUtil.GAN[(index + offset) % 10 + 1]
+        zhi = LunarUtil.ZHI[(index + LunarUtil.BASE_MONTH_ZHI_INDEX) % 12 + 1]
+        self.__ganZhi = gan + zhi
 
     def getIndex(self):
         return self.__index
@@ -25,41 +40,20 @@ class LiuYue:
     def getGanZhi(self):
         """
         获取干支
-        <p>
-        《五虎遁》
-        甲己之年丙作首，
-        乙庚之年戊为头，
-        丙辛之年寻庚上，
-        丁壬壬寅顺水流，
-        若问戊癸何处走，
-        甲寅之上好追求。
         :return: 干支
         """
-        offset = 0
-        year_gan_zhi = self.__liuNian.getGanZhi()
-        year_gan = year_gan_zhi[:1]
-        if "甲" == year_gan or "己" == year_gan:
-            offset = 2
-        elif "乙" == year_gan or "庚" == year_gan:
-            offset = 4
-        elif "丙" == year_gan or "辛" == year_gan:
-            offset = 6
-        elif "丁" == year_gan or "壬" == year_gan:
-            offset = 8
-        gan = LunarUtil.GAN[(self.__index + offset) % 10 + 1]
-        zhi = LunarUtil.ZHI[(self.__index + LunarUtil.BASE_MONTH_ZHI_INDEX) % 12 + 1]
-        return gan + zhi
+        return self.__ganZhi
 
     def getXun(self):
         """
         获取所在旬
         :return: 旬
         """
-        return LunarUtil.getXun(self.getGanZhi())
+        return LunarUtil.getXun(self.__ganZhi)
 
     def getXunKong(self):
         """
         获取旬空(空亡)
         :return: 旬空(空亡)
         """
-        return LunarUtil.getXunKong(self.getGanZhi())
+        return LunarUtil.getXunKong(self.__ganZhi)
